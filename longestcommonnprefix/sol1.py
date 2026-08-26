8697c5bd3c2eab91d45c51d9e4b62dc889cf572a
Kritika <kritswork9849@gmail.com>
2026-08-26 07:56:04 +0545

    solved longest common prefix problem

---
+++ longestcommonnprefix/sol1.py
@@ -0,0 +1,13 @@
+class Solution:
+    def longestCommonPrefix(self, strs):
+        prefix = strs[0]
+
+        for i in range(1, len(strs)):
+            while not strs[i].startswith(prefix):
+                prefix = prefix[:-1]
+                if prefix == "":
+                    return ""
+        return prefix
+
+s=Solution()
+print(s.longestCommonPrefix(["flower","flow","flight"]))
